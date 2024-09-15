356ceba87a7fe361b22a8b2a3b89a39604e19bfe
Subhan Mahmood <subhan70m@gmail.com>
2024-09-15 01:47:33 +0100

    chore: set theme to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
         <body>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange>
             <SessionProvider session={session}>
